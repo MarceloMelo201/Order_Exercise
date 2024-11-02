82edf8d40d3b3b686ee266cb840565aef40bc93d
Marcelo Melo <marcelovmelo201@gmail.com>
2024-11-02 18:53:00 -0300

    commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,56 @@
-package application;public class Program {
+package application;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter cliente data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Birth Date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.nextLine());
+
+        System.out.println("Enter order data:");
+        System.out.print("Status: ");
+        String status = sc.next();
+
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+
+        Order order = new Order(new Client(name, email, birthDate), OrderStatus.valueOf(status), new Date());
+
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter #" + (i+1) + " item data: ");
+            System.out.print("Product name: ");
+            String productName = sc.next();
+            System.out.print("Product price: ");
+            double price = sc.nextDouble();
+            System.out.print("Quantity: ");
+            int quantity = sc.nextInt();
+
+            order.addItem(new OrderItem(quantity, price, new Product(productName, price)));
+        }
+
+        System.out.println(order);
+
+        sc.close();
+
+    }
 }
--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,2 +1,88 @@
-package entities;public class Order {
+package entities;
+
+import entities.enums.OrderStatus;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Order {
+
+    private Date moment;
+    private OrderStatus status;
+    private Client client;
+    private List<OrderItem> orderItem = new ArrayList<>();
+    private static SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+    public Order(){}
+
+    public Order(Client client, OrderStatus status, Date moment) {
+        this.client = client;
+        this.status = status;
+        this.moment = moment;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public List<OrderItem> getOrderItem() {
+        return orderItem;
+    }
+
+    public void addItem(OrderItem item){
+        orderItem.add(item);
+    }
+
+    public void removeItem(OrderItem item){
+        orderItem.remove(item);
+    }
+
+    public Double total(){
+        double sum = 0;
+        for(OrderItem i: orderItem){
+            sum += i.subTotal();
+        }
+        return sum;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder print = new StringBuilder();
+        print.append("ORDER SUMMARY:")
+                .append("\nOrder moment: ").append(sdf1.format(moment))
+                .append("\nOrder status: ").append(status)
+                .append("\nClient: ").append(client.getName())
+                .append(" (").append(sdf1.format(moment)).append(") - ")
+                .append(client.getEmail())
+                .append("\nOrder items:");
+
+        for (OrderItem o : orderItem) {
+            print.append("\n").append(o.toString());
+        }
+
+        double total = total();
+
+        print.append("\nTotal price: $").append(total);
+        return print.toString();
+    }
+
+
 }
